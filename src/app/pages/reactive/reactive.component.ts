348e7fe49d467ec316900fc9e7b575f820475459
Jose Illan <jose_illan@hotmail.com>
2020-05-02 12:27:35 +0200

    Validator service and passwords fields validation

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { ValidadoresService } from 'src/app/services/validadores.service';
 
 @Component({
   selector: 'app-reactive',
@@ -10,7 +11,8 @@ export class ReactiveComponent implements OnInit {
 
   forma: FormGroup;
 
-  constructor( private fb: FormBuilder ) {
+  constructor( private fb: FormBuilder,
+               private validadores: ValidadoresService ) {
     this.crearFormulario();
     this.cargarDataAlFormulario();
   }
@@ -37,17 +39,30 @@ export class ReactiveComponent implements OnInit {
   get ciudadNoValido() {
     return this.forma.get('direccion.ciudad').invalid && this.forma.get('direccion.ciudad').touched
   }
+  get pass1NoValido() {
+    return this.forma.get('pass1').invalid && this.forma.get('pass1').touched
+  }
+  get pass2NoValido() {
+    const pass1 = this.forma.get('pass1').value;
+    const pass2 = this.forma.get('pass2').value;
+
+    return ( pass1 === pass2 ) ? false : true;
+  }
 
   crearFormulario() {
     this.forma = this.fb.group({
       nombre: ['', [ Validators.required, Validators.minLength(5) ] ],
-      apellido: ['', Validators.required ],
+      apellido: ['', [Validators.required, this.validadores.noHerrera] ],
       correo: ['', [ Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$') ]],
+      pass1: ['', Validators.required ],
+      pass2: ['', Validators.required ],
       direccion: this.fb.group({
         distrito: ['', Validators.required ],
         ciudad: ['', Validators.required ]
       }),
       pasatiempos: this.fb.array([])
+    }, {
+      validators: this.validadores.passwordsIguales('pass1', 'pass2')
     });
   }
 
@@ -59,8 +74,8 @@ export class ReactiveComponent implements OnInit {
         apellido: 'Illán',
         correo: 'jose@jose.com',
         direccion: {
-          distrito: 'FSDFSDF',
-          ciudad: 'sdfsdfsdf'
+          distrito: 'Ontario',
+          ciudad: 'Otawa'
         }
       }
     );
