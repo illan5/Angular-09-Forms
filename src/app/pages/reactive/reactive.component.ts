64e8a4a5f2f9904a4f66b54846bbad4f5e5d3997
Jose Illan <jose_illan@hotmail.com>
2020-05-02 10:35:20 +0200

    Fields group using formGroupName

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -26,12 +26,22 @@ export class ReactiveComponent implements OnInit {
   get correoNoValido() {
     return this.forma.get('correo').invalid && this.forma.get('correo').touched
   }
+  get distritoNoValido() {
+    return this.forma.get('direccion.distrito').invalid && this.forma.get('direccion.distrito').touched
+  }
+  get ciudadNoValido() {
+    return this.forma.get('direccion.ciudad').invalid && this.forma.get('direccion.ciudad').touched
+  }
 
   crearFormulario() {
     this.forma = this.fb.group({
       nombre: ['', [ Validators.required, Validators.minLength(5) ] ],
       apellido: ['', Validators.required ],
-      correo: ['', [ Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$') ]]
+      correo: ['', [ Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$') ]],
+      direccion: this.fb.group({
+        distrito: ['', Validators.required ],
+        ciudad: ['', Validators.required ]
+      })
     });
   }
 
@@ -39,6 +49,11 @@ export class ReactiveComponent implements OnInit {
     console.log( this.forma );
     if ( this.forma.invalid ) {
       return Object.values( this.forma.controls ).forEach( control => {
+        if ( control instanceof FormGroup ) {
+          Object.values( control.controls) .forEach( control => control.markAsTouched() );
+        } else {
+          control.markAsTouched();
+        }
         control.markAsTouched();
       });
     }
